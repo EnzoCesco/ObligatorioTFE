fa1a6cc3665f53915842490dba1fa8e6b8a167f6
EnzoCesco <62156609+EnzoCesco@users.noreply.github.com>
2020-08-10 21:09:00 -0300

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './login/login.js'
+import {createStore, combineReducers} from 'redux';
+import { Provider } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+
+  loginStatus : "guest",
+  apiKey : "",
+  id : "",
+  message : "",
+  loginErrorMessage : "",
+  taskList: []
+  
+};
+
+/* ---- REDUCERS ---- */ 
+
+function loginManager(state = initialState, action){
+  
+  let {responseCode, rApiKey, rId, rMessage} = {};
+
+  if(action.message !== undefined){
+    let {responseCode, rApiKey, rId, rMessage} = action.message;
+  }
+
+  switch(action.type){
+    case "LOGIN":
+      if(responseCode === 200){
+        return( { ...state, loginStatus: "loged", apiKey: rApiKey, id: rId, message : rMessage } );
+      }
+      else{
+        return( { ...state, loginStatus: "guest", apiKey: "", id: "", message : rMessage } );
+      }
+
+    default:
+      return state;
+
+  }
+}
+
+let rootReducer = combineReducers({loginManager});
+
+/* ---- CREATE STORE ---- */ 
+
+const store = createStore(rootReducer);
+
+/* ---- APP ---- */ 
+
+class App extends React.Component {
+
+  render(){
+
+      return (
+        <Provider store={store}>
+          <div className="loginScreen">
+            <div className="container">
+              <Login></Login>
+            </div>
+          </div>
+        </Provider>
+      );
+
+  }
+
 }
 
 export default App;
---
+++ src/login/login.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import './login.css';
+import {connect} from 'react-redux';
+
+export class Login extends React.Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+            user: "",
+            password: ""
+        }
+
+    }
+
+    handleUserChange = e => {
+        this.setState( { user: e.target.value } );
+    };
+
+    handlePasswordChange = e => {
+        this.setState( { password: e.target.value } );
+    };
+
+    validateLogin = () => {
+
+        let auth = { usuario : this.state.user , password : this.state.password };
+
+        fetch ("http://xpense.develotion.com/login.php",{
+            method: "POST",
+            body: JSON.stringify(auth),
+            headers:{'Content-Type': 'application/json'}
+        })
+            .then(res => res.json())
+            .then((result) => {
+                console.log(result);
+                if(result["codigo"] === 200){
+                    this.props.loginResult({
+                        responseCode : result["codigo"], 
+                        rApiKey: result["apiKey"],
+                        rId : result["id"]})
+                }
+                else{
+                    this.props.loginResult({
+                        responseCode : result["codigo"], 
+                        rApiKey: result["apiKey"],
+                        rId : result["id"],
+                        rMessage : result["mensaje"]})
+                }
+            })
+    };
+
+    render(){
+        return( 
+                <>
+                <div className="login">
+
+                    <label htmlFor="user" className="login">
+                        USUARIO :
+                        <input type="text" id="user" className="login" onChange={this.handleUserChange}></input>
+                    </label>
+                    
+                    <label htmlFor="password" className="login">
+                        CONTRASEÑA :
+                        <input type = "password" id="password" className="login" onChange={this.handlePasswordChange} ></input>
+                    </label>
+
+                    <input type="button"  value="INGRESAR" onClick={this.validateLogin}></input>
+                
+                    
+                </div>
+                <h4>{this.props.loginErrorMessage}</h4>
+                </>
+        )
+    }
+    ;
+
+}
+
+const  mapStateToProps = (state) => {
+    return{
+        loginErrorMessage : state.loginErrorMessage
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        loginResult: (result) => dispatch({type: "LOGIN", message: result})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
